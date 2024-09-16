977c82be1c7a8afa704c50c100aec0d1a14a8194
umer <choudhry00641@gmail.com>
2024-09-16 11:04:28 +0500

    Update code for feature X

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -28,7 +28,7 @@ export const Contact = () => {
     e.preventDefault();
     setButtonText("Sending...");
     try {
-      let response = await fetch("http://localhost:5000/contact", {
+      let response = await fetch("https://portfolio-yrnn-server-m3kwyj7p0-mumerch641s-projects.vercel.app/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
