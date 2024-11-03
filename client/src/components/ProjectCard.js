1993d7367c3ea182433b91e768d8bcc6e0367d13
umer <choudhry00641@gmail.com>
2024-11-03 23:56:20 +0500

    Initial commit

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,48 +1,17 @@
 
-// import { Col } from "react-bootstrap";
-
-// export const ProjectCard = ({ title, description, imgUrl }) => {
-//   return (
-//     <Col size={12} sm={6} md={4}>
-//       <div className="proj-imgbx">
-//         <img src={imgUrl} />
-//         <div className="proj-txtx">
-//           <h4>{title}</h4>
-//           <span>{description}</span>
-//         </div>
-//       </div>
-//     </Col>
-//   )
-// }
-// export default ProjectCard;
-
-
-
-
-
-
-
 import { Col } from "react-bootstrap";
-import { LazyLoadImage } from 'react-lazy-load-image-component'; // Import LazyLoadImage
-import 'react-lazy-load-image-component/src/effects/blur.css'; // Import optional effects
 
 export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <LazyLoadImage
-          src={imgUrl} // Image source
-          alt={title} // Alternative text
-          effect="blur" // Optional blur effect
-          style={{ width: '100%', height: 'auto' }} // Responsive styling
-        />
+        <img src={imgUrl} />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
       </div>
     </Col>
-  );
+  )
 }
-
-export default ProjectCard;
+export default ProjectCard;zzzz
