e91b887aa60a82cec570b6c291402000f9ad1270
umer <choudhry00641@gmail.com>
2024-11-04 10:25:50 +0500

    Your commit message here

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,17 +1,22 @@
-
+import React from 'react';
 import { Col } from "react-bootstrap";
-
+import { LazyLoadImage } from 'react-lazy-load-image-component'; // Import LazyLoadImage
 export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} />
+        <LazyLoadImage
+          src={imgUrl} // Replace img with LazyLoadImage
+          alt={title} // Add alt prop for accessibility
+          effect="blur" // Optional blur effect during loading
+          width={500} // Optional width for placeholder
+          height={250} // Optional height for placeholder
+        />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
       </div>
     </Col>
-  )
+  );
 }
-export default ProjectCard;zzzz
